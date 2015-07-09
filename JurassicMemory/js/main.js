b53087584946d7becaed678ba69842394724a64b
Jaytee Sanchez <jaytee.sanchez@gmail.com>
2015-07-08 20:56:11 -0700

    little updates need help undoing click event

--- JurassicMemory/js/main.js
+++ JurassicMemory/js/main.js
@@ -12,6 +12,7 @@ function makeGameBoard(){
   $(newImage).attr("class", "egg");
   $(newImage).attr("id", i);
   newImage.appendChild(imgli);
+  $gameBoard.css('background-color', 'rgba(0,0,0,0.7)');
  }
 
 }
@@ -51,11 +52,23 @@ function setImages(){
   $button.eq(i).css("background-image", "url(" + images[i] + ")");
 }
 }
+
+// event listener for the button that will start gameplay
+  var begin= document.getElementById('begin');
+  var header= document.getElementById('title');
+  begin.addEventListener('click', function(){
+    $(begin).css('display', 'none');
+    $(header).css('background-image', 'url("http://i.fonts2u.com/ju/jurassic-park.png")');
+    newGameStart();
+  });
+
 // Starting a New game
 function newGameStart(){
   makeGameBoard();
   getImages();
   nextMove();
+  var audio = new Audio('media/Jurassic Park.mp3');
+  audio.play();
 }
 // Determining round winner
 function getRoundWinner(matchCount1, matchCount2){
@@ -88,13 +101,17 @@ function getWinner(roundCount1, roundCount2){
 function checkMatch(){
   if(sClicks === 1){
     return;
+// var guess1= clickedSquares[0].outerHTML;
+// var guess2= clickedSquares[1].outerHTML;
+// console.log(guess1);
+// console.log(guess2);
 }else if(clickedSquares[0] === clickedSquares[1]){
     alert('match made!');
     matchCount();
 }else if(clickedSquares[0] !== clickedSquares[1]){
     alert('No Match, try again!');
     return clickedSquares;
-    reHide();
+    reHide(clickedSquares);
 }else{
   reHide();
 }
@@ -126,13 +143,14 @@ function matchCount(){
   }
 }
 // resetting the images if there is not match
-function reHide(){
+function reHide(clickedSquares){
   $(clickedSquares[0]).css('display', 'none');
   $(clickedSquares[0]).parent().toggleClass('egg');
   $(clickedSquares[1]).css('display', 'none');
   $(clickedSquares[1]).parent().toggleClass('egg');
   var sClicks= 0;
   var clickedSquares= [];
+  console.log('reHide works');
 }
 
 var clickedSquares= [];
@@ -140,26 +158,18 @@ var clickedSquares= [];
 // Click Counter
 var sClicks= 0;
 
-
 // event listener
-  mainSquare.addEventListener('click', function getClicks(event){
-    if(sClicks === 0){
+  mainSquare.addEventListener('click', function(event){
+    if(sClicks < 2){
+      console.log(event.srcElement.innerHTML);
       $(event.srcElement.children[0]).css('display', 'block');
       $(event.srcElement).toggleClass('egg');
-      clickedSquares.push($(event.srcElement.children[0]));
-      sClicks++;
-      console.log(clickedSquares);
-
-  }else if(sClicks === 1){
-      $(event.srcElement.children[0]).css('display', 'block');
-      $(event.srcElement).toggleClass('egg');
-      clickedSquares.push($(event.srcElement.children[0]));
+      clickedSquares.push(event.srcElement.children[0]);
       sClicks++;
       console.log(clickedSquares);
       checkMatch();
-  }else{
-    clicks= 0;
+    }else{
+    return;
   }
 });
-
 
